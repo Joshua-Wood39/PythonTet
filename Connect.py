3f7a26626bd10e11e38dc4767143193e6398753c
Joshua Wood <JerichoWall@apu.edu>
2020-04-03 14:02:52 -0700

    Starting new Connect project

---
+++ Connect.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+def create_board():
+    board = np.zeros((6, 7))
+    return board
+
+
+board = create_board()
+game_over = False
+turn = 0
+
+while not game_over:
+    # Ask for Player 1 input
+    if turn == 0:
+        selection = int(input("Player 1 Make Your Selection (0-6):"))
+
+    # Ask for Player 2 input
