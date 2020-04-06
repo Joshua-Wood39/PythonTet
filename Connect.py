0c55ce37de4490f9a72e3a9f4031b66a594805ef
Joshua Wood <JerichoWall@apu.edu>
2020-04-06 13:30:36 -0700

    Beginning to check win cons

--- Connect.py
+++ Connect.py
@@ -5,7 +5,7 @@ COLUMN_COUNT = 7
 
 
 def create_board():
-    board = np.zeros((6, 7))
+    board = np.zeros((ROW_COUNT, COLUMN_COUNT))
     return board
 
 
@@ -14,7 +14,7 @@ def drop_piece(board, row, col, piece):
 
 
 def is_valid_location(board, col):
-    return board[5][col] == 0
+    return board[ROW_COUNT-1][col] == 0
 
 
 def get_next_open_row(board, col):
@@ -25,6 +25,16 @@ def get_next_open_row(board, col):
 
 def print_board(board):
     print(np.flip(board, 0))
+
+
+def winning_move(board, piece):
+    # Check horizontal
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+
+    # Check vertical
 
 
 board = create_board()
