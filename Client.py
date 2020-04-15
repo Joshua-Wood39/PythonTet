656bf6a63d865df5d2209f239d7141511ccf231b
Joshua Wood <JerichoWall@apu.edu>
2020-04-15 14:54:00 -0700

    Finished client update; moving on to server

--- Client.py
+++ Client.py
@@ -37,6 +37,9 @@ class Player():
         if keys[pygame.K_DOWN]:
             self.y += self.vel
 
+        self.update()
+
+    def update(self):
         self.rect = (self.x, self.y, self.width, self.height)
 
 
@@ -49,29 +52,36 @@ def make_pos(tup):
     return str(tup[0]) + "," + str(tup[1])
 
 
-def redraw_window(win, player):
+def redraw_window(win, player, player2):
 
     win.fill((255, 255, 255))
     player.draw(win)
+    player2.draw(win)
     pygame.display.update()
 
 
 def main():
     run = True
     n = Network()
-    startPos = n.getPos()
-    p = Player(50, 50, 100, 100, (0, 255, 0))
+    startPos = read_pos(n.getPos())
+    p = Player(startPos[0], startPos[1], 100, 100, (0, 255, 0))
+    p2 = Player(0, 0, 100, 100, (0, 255, 0))
     clock = pygame.time.Clock()
 
     while run:
         clock.tick(60)
+        p2Pos = read_pos(n.send(make_pos((p.x, p.y))))
+        p2.x = p2Pos[0]
+        p2.y = p2Pos[1]
+        p2.update()
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 pygame.quit()
 
         p.move()
-        redraw_window(win, p)
+        redraw_window(win, p, p2)
 
 
 main()
