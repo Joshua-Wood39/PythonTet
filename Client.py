337f19902902523275503600f82dc2615f32c087
Joshua Wood <JerichoWall@apu.edu>
2020-04-17 15:17:08 -0700

    Client base structure ready, working on interaction with server

--- Client.py
+++ Client.py
@@ -1,41 +1,59 @@
 import pygame
 from Network import Network
 from player import Player
+pygame.font.init()
 
-width = 500
-height = 500
+width = 700
+height = 700
 win = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Client")
 
-clientNumber = 0
+
+class Button:
+    def __init__(self, text, x, y, color):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.color = color
+        self.width = 150
+        self.height = 100
+
+    def draw(self, win):
+        pygame.draw.rect(
+            win, self.color, (self.x, self.y, self.width, self.height))
+        font = pygame.font.SysFont("comicsans", 40)
+        text = font.render(self.text, 1, (255, 255, 255))
+        win.blit(text, (self.x + round(self.width/2) - round(text.get_width()/2),
+                        self.y + round(self.height/2) - round(text.get_height()/2)))
+
+    def click(self, pos):
+        x1 = pos[0]
+        y1 = pos[1]
+        if self.x <= x1 <= self.x + self.width and self.y <= y1 <= self.y + self.height:
+            return True
+        else:
+            return False
 
 
-def redraw_window(win, player, player2):
+def redraw_window(win, game, p):
 
-    win.fill((255, 255, 255))
-    player.draw(win)
-    player2.draw(win)
-    pygame.display.update()
+    win.fill((128, 128, 128))
+    pass
+
+
+btns = [Button("Rock", 50, 500, (0, 0, 0)), Button(
+    "Paper", 250, 500, (255, 255, 255)), Button("Scissors", 450, 500, (145, 145, 145))]
 
 
 def main():
     run = True
-    n = Network()
-    p = n.getP()
-
     clock = pygame.time.Clock()
+    n = Network()
+    player = int(n.getP())
+    print("You are player: ", player)
 
     while run:
         clock.tick(60)
-        p2 = n.send(p)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-        p.move()
-        redraw_window(win, p, p2)
 
 
 main()
